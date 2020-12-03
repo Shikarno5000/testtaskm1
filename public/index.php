ad7cd94fc00429da0965df39bca09dda13c83350
Andrey Martinov <andrey@martinov.ru>
2020-12-03 15:19:05 +0300

    ДОбавляет внешний файл

---
+++ public/index.php
@@ -0,0 +1,14 @@
+<?php
+
+print_r($_POST);
+
+?>
+<html>
+  <head></head>
+  <body>
+    <form action="" method="POST">
+      <input type="text" name="incomingLine"/>
+      <button type="submit">Отправить</button>
+    </form>
+  </body>
+</html>
